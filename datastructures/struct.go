100f11346782957d1a2fddae4d2cb918b5f6d664
Bogdandabeast <bogdan.console@gmail.com>
2024-10-12 18:13:50 +0200

    Ligma

---
+++ datastructures/struct.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type Vertex struct { //Sctuct es una coleccion de campos 
+	X int
+	Y int
+}
+
+func main() {
+	v := Vertex{1, 2} //Se instancia Vertex con valores x = 1 y y = 2
+	v.X = 4 //Se asigna valor 4 a la variable x de Vertex de v
+	fmt.Printf("Los valores son %v y %v", v.X, v.Y)
+}
